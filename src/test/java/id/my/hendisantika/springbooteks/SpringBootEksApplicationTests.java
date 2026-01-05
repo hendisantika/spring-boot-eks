217512c94afe201ee2b2f82da252c23445fa7ea6
hendisantika <hendisantika@yahoo.co.id>
2026-01-06 06:56:50 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳

---
+++ src/test/java/id/my/hendisantika/springbooteks/SpringBootEksApplicationTests.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbooteks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootEksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
