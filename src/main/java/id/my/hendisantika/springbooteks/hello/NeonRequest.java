1e6853f6be4baa48e13ce4cb55c490c5de6e399a
hendisantika <hendisantika@yahoo.co.id>
2026-01-06 07:02:17 +0700

    feat: Add NeonRequest🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbooteks/hello/NeonRequest.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.springbooteks.hello;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-eks
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 06/01/26
+ * Time: 07.01
+ * To change this template use File | Settings | File Templates.
+ */
+public class NeonRequest {
+    private String name;
+    private Double value;
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public Double getValue() { return value; }
+    public void setValue(Double value) { this.value = value; }
+}
