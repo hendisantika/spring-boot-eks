d5dd6bc0bf7110aacc9fd3b90b291670a571c2cf
hendisantika <hendisantika@yahoo.co.id>
2026-01-06 07:03:12 +0700

    feat: Add PlayingWithNeonRepository🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbooteks/hello/PlayingWithNeonRepository.java
@@ -0,0 +1,17 @@
+package id.my.hendisantika.springbooteks.hello;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-eks
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 06/01/26
+ * Time: 07.02
+ * To change this template use File | Settings | File Templates.
+ */
+public interface PlayingWithNeonRepository extends JpaRepository<PlayingWithNeon, Long> {
+}
